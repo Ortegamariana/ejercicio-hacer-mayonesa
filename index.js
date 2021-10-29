b4e9f4f3bee9c1711d9bb42328378da97f87a745
Ortegamariana <ortegamariana.15@gmail.com>
2021-10-29 19:08:42 -0300

    Clase Mayonesa

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+class Mayonesa {
+    constructor (receta, ingredientes, utensilios, cantidades) {
+      this.receta = receta;
+      this.ingredientes = ingredientes;
+      this.utensilios = utensilios;
+      this.cantidades =cantidades;
+    }
+    // Getter
+    get ingredientes() {
+       return this.calcCantidades();
+     }
+    // Método
+    calcCantidades () {
+      return this.cantidades ;
+    }
+  }
+
 
 console.log("MAYONESA SEGUN YO");
 
